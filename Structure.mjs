00f9466af4700621b5dcea085545c831c2639c7e
Fahrifu <82388717+Fahrifu@users.noreply.github.com>
2025-02-22 12:56:12 +0100

    Base case function analyzeTree

---
+++ Structure.mjs
@@ -0,0 +1,13 @@
+function analyzeTree(node, depth = 1) {
+    if (!node) {
+        return {
+            sum: 0,
+            depth: depth - 1,
+            count: 0
+        };
+    }
+
+    let left = analyzeTree(node.left, depth + 1);
+    let right = analyzeTree(node.right, depth + 1);
+    
+}
